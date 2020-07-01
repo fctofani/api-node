c3138206a0cb1257be61349660e0154fe70afba1
Fabricio Tofani <fctofani2@gmail.com>
2020-07-01 15:00:04 -0300

    chore: add mongo configs

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,11 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
-
-  // The test environment that will be used for testing
   testEnvironment: 'node',
-
-  // A map from regular expressions to paths to transformers
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
